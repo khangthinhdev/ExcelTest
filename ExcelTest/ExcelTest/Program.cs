28638981686dc6bfd28df1c09bd3ff2eb251836f
Khang Thịnh Nguyễn <khangthinh.dev@gmail.com>
2020-06-07 18:49:28 +0700

    Commit ne

--- ExcelTest/ExcelTest/Program.cs
+++ ExcelTest/ExcelTest/Program.cs
@@ -7,10 +7,6 @@ namespace ExcelTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // this is comment
-            Console.WriteLine("Comment");
-            // this is another comment
-            Console.WriteLine("This is another comment");
         }
     }
 }
